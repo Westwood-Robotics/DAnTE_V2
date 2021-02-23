a1e7fdea951674d951c071e16eb5ce2201e8bb5d
Hawkry Zhang <hawkrybruin@gmail.com>
2021-02-22 17:32:21 -0800

    --

--- forward_kin_test.py
+++ forward_kin_test.py
@@ -4,5 +4,5 @@ from Play.robot_controller import RobotController
 rc = RobotController(robot=DAnTE)
 rc.start_robot()
 rc.initialization()
-
+usr = input("Press any key and enter to continue...")
 rc.update_angles()
